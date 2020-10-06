b992207e209dcbd79a2f9712a4676fbbb5f6d7ad
rouben.rehman <rouben.rehman@gmail.com>
2020-10-06 15:28:13 +0200

    Added result page creation to ci

--- shell/create_hugo_header.py
+++ shell/create_hugo_header.py
@@ -3,6 +3,6 @@ import sys
 front_matter = "---\ndate: "\
                 + str(sys.argv[1])\
                 + "\ntitle: \"" + str(sys.argv[2])\
-                + "\"\nlinkTitle: \"" + str(sys.argv[3])\
+                + "\"\nlinkTitle: \"" + str(sys.argv[2]) + " " + str(sys.argv[1])\
                 + "\"\nresources:\n- src: \"./logs/**.{png,jpg}\"\n  title: \"Image #:counter\"\n  params:\n    byline:\"Results of train run\"\n---"
 print(front_matter)
