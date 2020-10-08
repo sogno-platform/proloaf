b5e3de334773c92cd1a38f1df20e4f304ce6e08e
rouben.rehman <rouben.rehman@gmail.com>
2020-10-08 18:24:12 +0200

    Fixed create_hugo_header.py

--- shell/create_hugo_header.py
+++ shell/create_hugo_header.py
@@ -4,5 +4,5 @@ front_matter = "---\ndate: "\
                 + str(sys.argv[1])\
                 + "\ntitle: \"" + str(sys.argv[2])\
                 + "\"\nlinkTitle: \"" + str(sys.argv[2]) + " " + str(sys.argv[1])\
-                + "\"\nresources:\n- src: \"./logs/**.{png,jpg}\"\n  title: \"Image #:counter\"\n  params:\n    byline:\"Results of train run\"\n---"
+                + "\"\nresources:\n- src: \"./**.{png,jpg}\"\n  title: \"Image #:counter\"\n  params:\n    byline:\"Results of train run\"\n---"
 print(front_matter)
