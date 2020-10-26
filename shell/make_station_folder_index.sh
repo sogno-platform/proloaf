7e58922ad08022ceb5a3f115ebf0d4696b560b5e
rouben.rehman <rouben.rehman@gmail.com>
2020-10-26 16:06:01 +0100

    Testing recurrent folder structure generation

--- shell/make_station_folder_index.sh
+++ shell/make_station_folder_index.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-if test ! -f "./repo/$STATION/_index.md"; then
-  echo "$STATION"
-  touch ./repo/$STATION/_index.md
-  python ./shell/create_hugo_header.py --title=$STATION --link=$STATION >> ./repo/$STATION/_index.md
-  echo "Results for $STATION" >> ./repo/$STATION/_index.md
+if test ! -f "./repo/$1/_index.md"; then
+  touch ./repo/$1/_index.md
+  python ./shell/create_hugo_header.py --title=$1 --link=$1 >> ./repo/$1/_index.md
+  echo "Results for $1" >> ./repo/$1/_index.md
 fi
