422824859b96d096115fec2a3adcc72671fdc2fa
GuersesTran <gguerses@eonerc.rwth-aachen.de>
2021-06-04 16:53:34 +0200

    remove seperate license files from former repos
    Signed-off-by: GuersesTran <gguerses@eonerc.rwth-aachen.de>

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 if __name__ == '__main__':
     setup(
+        name='utils',
+        version='0.1.0',
         install_requires = [
             'numpy',
             'pandas',
