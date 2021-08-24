0010cb719716fce5ce5316fcb8882880b47b9641
GuersesTran <gguerses@eonerc.rwth-aachen.de>
2021-08-24 05:45:04 +0200

    fix CI after renamed scripts
    Signed-off-by: GuersesTran <gguerses@eonerc.rwth-aachen.de>

--- shell/trigger_training.sh
+++ shell/trigger_training.sh
@@ -24,5 +24,5 @@ then
     echo "No station information given, contain the station name in the \$STATION variable"
 else
     echo "Starting training for ${STATION}"
-    python3 source/fc_train.py -s ${STATION} --ci
+    python3 source/train.py -s ${STATION} --ci
 fi
