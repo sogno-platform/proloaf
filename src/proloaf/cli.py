c1370c839f5837bb0063771ce193ad1afc252d57
GuersesTran <gguerses@eonerc.rwth-aachen.de>
2021-11-10 17:32:07 +0100

    set root folder to src
    Signed-off-by: GuersesTran <gguerses@eonerc.rwth-aachen.de>

---
+++ src/proloaf/cli.py
@@ -0,0 +1,286 @@
+# Copyright 2021 The ProLoaF Authors. All Rights Reserved.
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# ==============================================================================
+"""
+Provides various functions to run command line interface.
+"""
+
+import sys
+import argparse
+import proloaf.metrics as metrics
+
+
+class flag_and_store(argparse._StoreAction):
+    def __init__(self, option_strings, dest, dest_const, const, nargs=0, **kwargs):
+        self.dest_const = dest_const
+        self.flag = const
+        super(flag_and_store, self).__init__(
+            option_strings, dest, const=None, nargs=nargs, **kwargs
+        )
+        self.nargs = nargs
+
+    def __call__(self, parser, namespace, values, option_strings=None):
+        setattr(namespace, self.dest_const, self.flag)
+        super().__call__(parser, namespace, values, option_strings)
+
+
+def parse_basic(args=sys.argv[1:]):
+    """
+    Parse command line arguments and store them in attributes of a Python object.
+
+    Accepts (only) one of the following options:
+
+    - "-s", "--station", the name of the station to be trained for. 'gefcom2017/nh_data' by default.
+    - "-c", "--config", the path to the config file relative to the project root
+
+    Parameters
+    ----------
+    args : list
+        A list containing all command line arguments starting with argv[1].
+
+    Returns
+    -------
+    Namespace
+        An object that stores the given values of parsed arguments as attributes
+    """
+
+    parser = argparse.ArgumentParser()
+    # TODO this should be a required argument (also remove default below)
+    ident = parser.add_mutually_exclusive_group()  # required=True)
+    ident.add_argument(
+        "-s",
+        "--station",
+        help="station to be trained for (e.g. gefcom2017/nh_data)",
+        default="gefcom2017/nh_data",
+    )
+    ident.add_argument(
+        "-c", "--config", help="path to the config file relative to the project root"
+    )
+    return parser.parse_args(args)
+
+
+def parse_with_loss(args=sys.argv[1:]):
+    """
+    Parse command line arguments, including arguments for the loss function, and store them in attributes of a Python object.
+
+    Accepts the following options:
+
+    - "--ci", Enables execution mode optimized for GitLab's CI
+    - "--logname", Name of the run, displayed in Tensorboard
+
+    AND (only) one of the following options:
+
+    - "-s", "--station", the name of the station to be trained for. 'gefcom2017/nh_data' by default.
+    - "-c", "--config", the path to the config file relative to the project root
+
+    AND (only) one of the following options (which gets stored in the 'loss' attribute as a callable):
+
+    - "--nll_gauss", Enables training with nll guassian loss
+    - "--quantiles", Enables training with pinball loss and MSE with q1 and q2 being the upper and lower quantiles.
+    Requires at least 2 float arguments e.g. "--quantiles q1, q2, ..."
+    - "--crps","--crps_gaussian", Enables training with crps gaussian loss
+    - "--mse", "--mean_squared_error", Enables training with mean squared error
+    - "--rmse", "--root_mean_squared_error", Enables training with root mean squared error
+    - "--mape", Enables training with root mean absolute error (mean absolute percentage error in %)
+    - "--sharpness", Enables training with sharpness
+    - "--mis", "--mean_interval_score", Enables training with mean interval score. Requires a single float as an
+    argument e.g. "--mis alpha" where alpha is a float value.
+
+    Parameters
+    ----------
+    args : list
+        A list containing all command line arguments starting with argv[1].
+
+    Returns
+    -------
+    Namespace
+        An object that stores the following attributes:
+
+        - ci : True/False (see function comment)
+        - logname : A string (see function comment), '' by default
+        - station : A string (see function comment) e.g. 'gefcom2017/nh_data'
+        - config : A string (see function comment), None by default
+        - num_pred : An int with the number of predictions
+        - quantiles : A list of floats, if '--quantiles' was used, otherwise None
+        - alpha : A float, if '--mis' was used, otherwise None
+        - loss : The specified callable loss function from proloaf.models.py
+    dict
+        Contains extra options if the loss functions mis or quantile score are used.
+
+        - if '--mis a' is used, where 'a' is a float: the dict has a single entry i.e. {'alpha': a }
+        - if "--quantiles q1 q2 ..." is used, where q1, q2,... are a series of (at least 2) floats: the dict contains a
+        list with the entries q1, q2, ... i.e. {'quantiles': [q1, q2, ...]}
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--ci",
+        help="Enables execution mode optimized for GitLab's CI",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--logname",
+        help="Name of the run, displayed in Tensorboard",
+        type=str,
+        action="store",
+        default="",
+    )
+
+    # TODO this should be a required argument (also remove default below)
+    ident = parser.add_mutually_exclusive_group()  # required=True)
+    ident.add_argument(
+        "-s",
+        "--station",
+        help="station to be trained for (e.g. gefcom2017/nh_data)",
+        default="gefcom2017/nh_data",
+    )
+    ident.add_argument(
+        "-c", "--config", help="path to the config file relative to the project root"
+    )
+
+    losses = parser.add_mutually_exclusive_group()
+    # losses.add_argument("--hyper", help="turn hyperparam-tuning on/off next time (int: 1=on, else=off)", type=int, default=0)
+    # losses.add_argument("-o", "--overwrite", help = "overwrite config with new training parameter (int: 1=True=default/else=False)", type=int, default=0)
+    losses.add_argument(
+        "--nll_gauss",
+        dest="loss",
+        const="NllGauss",
+        help="train with nll guassian loss",
+        action="store_const",
+    )
+    losses.add_argument(
+        "--quantiles",
+        dest_const="loss",
+        dest="loss_arguments",
+        metavar=" q1 q2",
+        nargs="+",
+        type=float,
+        const="QuantileScore",
+        help="train with pinball loss and MSE with q1 and q2 being the upper and lower quantiles",
+        action=flag_and_store,
+    )
+    losses.add_argument(
+        "--crps",
+        "--crps_gaussian",
+        dest="loss",
+        const="CRPSGauss",
+        help="train with crps gaussian loss",
+        action="store_const",
+    )
+    losses.add_argument(
+        "--mse",
+        "--mean_squared_error",
+        dest="loss",
+        const="Mse",
+        help="train with mean squared error",
+        action="store_const",
+    )
+    losses.add_argument(
+        "--rmse",
+        "--root_mean_squared_error",
+        dest="loss",
+        const="Rmse",
+        help="train with root mean squared error",
+        action="store_const",
+    )
+    losses.add_argument(
+        "--mape",
+        dest="loss",
+        const="Mape",
+        help="train with root mean absolute error",
+        action="store_const",
+    )
+    # losses.add_argument("--mase", dest_const='loss', dest='num_pred', nargs=0, type=int, const=[metrics.mase,2], help = "train with root mean absolute scaled error", action=flag_and_store)
+    # losses.add_argument("--picp", dest_const='loss', dest='num_pred', nargs=0, type=int, const=[metrics.picp_loss,2], help = "train with 1 - prediction interval coverage",action=flag_and_store)
+    losses.add_argument(
+        "--sharpness",
+        dest="loss",
+        const="Shaprness",
+        help="train with sharpness",
+        action="store_const",
+    )
+    losses.add_argument(
+        "--mis",
+        "--mean_interval_score",
+        dest_const="loss",
+        dest="loss_arguments",
+        nargs=1,
+        metavar="A",
+        type=float,
+        const="Mis",
+        help="train with mean interval score. 'A' corresponds to the inverse weight",
+        action=flag_and_store,
+    )
+    # TODO write mis as default argument as soon as evaluate works with it
+    parser.set_defaults(loss="NllGauss")
+    ret = parser.parse_args(args)
+
+    if ret.loss.lower() == "mis":
+        options = dict(alpha=ret.loss_arguments)
+    elif ret.loss.lower() == "quantilescore":
+        options = dict(quantiles=ret.loss_argumets)
+    else:
+        options = {}
+    return ret, options
+
+
+def query_true_false(question, default="yes"):
+    """
+    Ask a yes/no question via raw_input() and return their answer.
+
+    Parameters
+    ----------
+    question : string
+        A string with the question that is presented to the user
+    default : string or None default = "yes"
+        The presumed answer if the user just hits <Enter>. Valid string values are "yes" or "no". None means an
+        answer is required of the user.
+
+    Returns
+    -------
+    bool
+        True for "yes" or False for "no".
+
+    """
+
+    ## Source: https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
+    # valid = {"yes": False, "y": False, "ye": False,
+    #         "no": True, "n": True}
+
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
---
+++ src/proloaf/plot.py
@@ -0,0 +1,375 @@
+# Copyright 2021 The ProLoaF Authors. All Rights Reserved.
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# ==============================================================================
+"""
+Provides handy plot functions to visualize predictions and performancec over selected timeseries
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import proloaf.metrics as metrics
+
+
+def plot_timestep(
+    target,
+    pred,
+    y_pred_upper,
+    y_pred_lower,
+    timestep,
+    OUTPATH,
+    limit,
+    actual_time=None,
+    draw_limit=False,
+):
+    """
+    Create a matplotlib.pyplot.subplot to compare true and predicted values.
+    Save the resulting plot at (OUTPATH + 'eval_hour{}'.format(timestep))
+
+    Parameters
+    ----------
+    target : ndarray
+        Numpy array containing true values
+    pred : ndarray
+        Numpy array containing predicted values
+    y_pred_upper : ndarray
+        Numpy array containing upper limit of prediction confidence interval
+    y_pred_lower : ndarray
+        Numpy array containing lower limit of prediction confidence interval
+    timestep : int
+        The timestep of the prediction, relative to the beginning of the dataset
+    OUTPATH : string
+        Path to where the plot should be saved
+    limit : float
+        The cap limit. Used to draw a horizontal line with height = limit.
+    actual_time : pandas.Series, default = None
+        The actual time from the data set
+
+    Returns
+    -------
+    No return value
+    """
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(target, ".-k", label="Truth")  # true values
+    ax.plot(pred, "b", label="Predicted")
+    # insert actual time, assuming the underlying data to be in hourly resolution
+    try:
+        ax.set_title(actual_time.iloc[0].strftime("%a, %Y-%m-%d"), fontsize=20)
+        positions = range(0, len(pred), 2)
+        labels = (
+            actual_time.dt.hour.to_numpy()
+        )  # assuming hourly resolution in most of the evaluations
+        new_labels = labels[positions]
+        plt.xticks(positions, new_labels)
+    except:
+        ax.set_title("Forecast along horizon", fontsize=22)
+    ax.fill_between(
+        np.arange(pred.shape[0]),
+        pred.squeeze(),
+        y_pred_upper.squeeze(),
+        alpha=0.1,
+        color="g",
+    )
+    ax.fill_between(
+        np.arange(pred.shape[0]),
+        y_pred_lower.squeeze(),
+        pred.squeeze(),
+        alpha=0.1,
+        color="g",
+    )
+
+    ax.set_xlabel("Hour", fontsize=18)
+    ax.set_ylabel("Scaled Residual Load (-1,1)", fontsize=20)
+    ax.legend(fontsize=20)
+    ax.grid(b=True, linestyle="-")
+    if limit and draw_limit:
+        plt.axhline(linewidth=2, color="r", y=limit)
+    ax.grid()
+    ax.set_xlabel(
+        "Time of Day", fontsize=20
+    )  # assuming hourly resolution in most of the evaluations
+    plt.autoscale(enable=True, axis="x", tight=True)
+    plt.savefig(OUTPATH + "eval_hour{}".format(timestep))
+    # plt.show(fig)
+    plt.close(fig)
+
+
+def plot_metrics(
+    rmse_horizon, sharpness_horizon, coverage_horizon, mis_horizon, OUTPATH, title=""
+):
+    """
+    Create a matplotlib.pyplot.figure with plots for the given metrics
+    Save the resulting figure at (OUTPATH + 'metrics_plot')
+
+    Parameters
+    ----------
+    rmse_horizon : ndarray
+        The values for the root mean square error over the horizon
+    sharpness_horizon : ndarray
+        The values for the sharpness over the horizon
+    coverage_horizon : ndarray
+        The values for the PICP (prediction interval coverage probability or % of true
+        values in the predicted intervals) over the horizon
+    mis_horizon : ndarray
+        The values for the mean interval score over the horizon
+    OUTPATH : string
+        The path to where the figure should be saved
+    title : string
+        The text for a centered title for the figure
+
+    Returns
+    -------
+    No return value
+    """
+
+    with plt.style.context("seaborn"):
+        fig = plt.figure(figsize=(16, 12))
+        st = fig.suptitle(title, fontsize=25)
+        plt.rc("xtick", labelsize=15)
+        plt.rc("ytick", labelsize=15)
+
+        ax_rmse = plt.subplot(2, 2, 1)
+        ax_sharpness = plt.subplot(2, 2, 3)
+        ax_PICP = plt.subplot(2, 2, 2)
+        ax_MIS = plt.subplot(2, 2, 4)
+
+        for element in rmse_horizon:
+            ax_rmse.plot(rmse_horizon[element], label=element)
+        ax_rmse.set_title("RMSE along horizon", fontsize=22)
+        ax_rmse.set_xlabel("Hour", fontsize=18)
+        ax_rmse.set_ylabel("RMSE", fontsize=20)
+        ax_rmse.legend(fontsize=20)
+        ax_rmse.grid(b=True, linestyle="-")
+
+        for element in sharpness_horizon:
+            ax_sharpness.plot(sharpness_horizon[element], label=element)
+        ax_sharpness.set_title("sharpness along horizon", fontsize=22)
+        ax_sharpness.set_xlabel("Hour", fontsize=18)
+        ax_sharpness.set_ylabel("sharpness", fontsize=20)
+        ax_sharpness.legend(fontsize=20)
+        ax_sharpness.grid(b=True, linestyle="-")
+
+        for element in coverage_horizon:
+            ax_PICP.plot(coverage_horizon[element], label=element)
+        ax_PICP.set_title("coverage along horizon", fontsize=22)
+        ax_PICP.set_xlabel("Hour", fontsize=18)
+        ax_PICP.set_ylabel("coverage in %", fontsize=20)
+        ax_PICP.legend(fontsize=20)
+        ax_PICP.grid(b=True, linestyle="-")
+
+        for element in mis_horizon:
+            ax_MIS.plot(mis_horizon[element], label=element)
+        ax_MIS.set_title("Mean Interval score", fontsize=22)
+        ax_MIS.set_xlabel("Hour", fontsize=18)
+        ax_MIS.set_ylabel("MIS", fontsize=20)
+        ax_MIS.legend(fontsize=20)
+        ax_MIS.grid(b=True, linestyle="-")
+
+        st.set_y(1.08)
+        fig.subplots_adjust(top=0.95)
+        plt.tight_layout()
+        plt.savefig(OUTPATH + title + "_metrics_plot")
+        plt.show()
+
+
+def plot_boxplot(
+    metrics_per_sample: pd.DataFrame,
+    sample_frequency=1,
+    save_to_disc=False,
+    fig_title="boxplot",
+):
+    """
+    Create a matplotlib.pyplot.figure with boxplots for the given metrics
+    Save the resulting figure at (save_to_disc + 'boxplot')
+    To visualize more clearly a sampling frequency can be defined,
+    i.e. the time-delta of the moving windows over the test set.
+    Per default over the test set, every timestep initiates a new out-of-sample prediction.
+    E.g. if the evaluations hall happen on daily basis with hourly time steps sample_frequency can be set to 24.
+
+    Parameters
+    ----------
+    TODO
+
+    Returns
+    -------
+    No return value
+    """
+    fig = plt.figure()  # plt.figure(figsize=(16, 12))
+    ax1 = metrics_per_sample.iloc[::sample_frequency].boxplot(
+        # column=analyzed_metrics,  # list(metrics_per_sample.keys()),
+        color=dict(boxes="k", whiskers="k", medians="k", caps="k"),
+        # figsize=(8.5, 10),fontsize=24,
+    )
+    ax1.set_xlabel(
+        "Mean error per sample with predefined sampling steps: "
+        + str(sample_frequency)
+        + " on prediction horizon",
+    )  # fontsize=24
+    ax1.set_ylabel("Error measure")  # , fontsize=24
+    # ymin, ymax = -0.01, 1.5
+    # ax1.set_ylim([ymin, ymax])
+    if save_to_disc:
+        plt.savefig(save_to_disc + fig_title + ".png")
+    plt.show()
+
+
+def plot_hist(
+    targets,
+    expected_values,
+    y_pred_upper,
+    y_pred_lower,
+    analyzed_metrics=["mse"],
+    sample_frequency=1,
+    save_to_disc=False,
+    fig_title="Error Probability Distribution",
+    method="method",
+    bins=10,
+):
+    """
+    Create a matplotlib.pyplot.figure with kde plots for the given metrics
+    Save the resulting figure at (save_to_disc + 'Error Probability Distribution')
+    To visualize more clearly a sampling frequency can be defined,
+    i.e. the time-delta of the moving windows over the test set.
+    Per default over the test set, every timestep initiates a new out-of-sample prediction.
+    E.g. if the evaluations hall happen on daily basis with hourly time steps sample_frequency can be set to 24.
+
+    Parameters
+    ----------
+    TODO
+
+    Returns
+    -------
+    No return value
+    """
+    metrics_per_sample = {}
+    for results in range(targets.shape[0]):
+        mse_per_sample = [
+            metrics.mse(targets[results][i], [expected_values[results][i]])
+            for i, value in enumerate(targets[results])
+        ]
+        metrics_per_sample[results] = pd.DataFrame(
+            data=mse_per_sample, columns=["mse"]
+        ).astype("float")
+        if "rmse" in analyzed_metrics:
+            rmse_per_sample = [
+                metrics.rmse(targets[results][i], [expected_values[results][i]])
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["rmse"] = pd.DataFrame(rmse_per_sample).astype(
+                "float"
+            )
+        if "sharpness" in analyzed_metrics:
+            sharpness_per_sample = [
+                metrics.sharpness([y_pred_upper[results][i], y_pred_lower[results][i]])
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["sharpness"] = pd.DataFrame(
+                sharpness_per_sample
+            ).astype("float")
+        if "picp" in analyzed_metrics:
+            coverage_per_sample = [
+                metrics.picp(
+                    targets[i], [y_pred_upper[results][i], y_pred_lower[results][i]]
+                )
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["picp"] = pd.DataFrame(
+                coverage_per_sample
+            ).astype("float")
+        if "rae" in analyzed_metrics:
+            rae_per_sample = [
+                metrics.rae(targets[i], [expected_values[results][i]])
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["rae"] = pd.DataFrame(rae_per_sample).astype(
+                "float"
+            )
+        if "mae" in analyzed_metrics:
+            mae_per_sample = [
+                metrics.mae(targets[i], [expected_values[results][i]])
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["mae"] = pd.DataFrame(mae_per_sample).astype(
+                "float"
+            )
+        if "mis" in analyzed_metrics:
+            mis_per_sample = [
+                metrics.mis(
+                    targets[results][i],
+                    [y_pred_upper[results][i], y_pred_lower[results][i]],
+                    alpha=0.05,
+                )
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["mis"] = pd.DataFrame(mis_per_sample).astype(
+                "float"
+            )
+        if "mase" in analyzed_metrics:
+            mase_per_sample = [
+                metrics.mase(
+                    targets[results][i],
+                    [expected_values[results][i]],
+                    0,
+                    insample_target=targets[results].roll(7 * 24, 0)[i],
+                )
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["mase"] = pd.DataFrame(mase_per_sample).astype(
+                "float"
+            )
+        if "residuals" in analyzed_metrics:
+            residuals_per_sample = [
+                metrics.residuals(targets[results][i], [expected_values[results][i]])
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["residuals"] = pd.DataFrame(
+                residuals_per_sample
+            ).astype("float")
+        if "qs" in analyzed_metrics:
+            quantile_score_per_sample = [
+                metrics.pinball_loss(
+                    targets[i],
+                    [y_pred_upper[results][i], y_pred_lower[results][i]],
+                    [0.025, 0.975],
+                )
+                for i, value in enumerate(targets[results])
+            ]
+            metrics_per_sample[results]["qs"] = pd.DataFrame(
+                quantile_score_per_sample
+            ).astype("float")
+    all_results = pd.concat(
+        metrics_per_sample.values(), keys=metrics_per_sample.keys(), axis=1
+    )
+    with plt.style.context("seaborn"):
+        fig = plt.figure(figsize=(16, 12))  # plt.figure()
+        results = pd.DataFrame(
+            data=all_results.xs("residuals", axis=1, level=1, drop_level=False).values,
+            columns=method,
+        )
+        # print("Residuals DataFrame Head:",results.head())
+        for element in results:
+            ax1 = results[element].plot(
+                kind="hist", bins=bins, alpha=0.5, label=element
+            )
+            ax1.set_title("Histogram of Residuals", fontsize=22)
+            ax1.set_xlabel("Residuals", fontsize=18)
+            ax1.set_ylabel("Frequency", fontsize=20)
+            ax1.legend(fontsize=20, loc="upper center", fancybox=True)
+        if save_to_disc:
+            plt.savefig(save_to_disc + fig_title + ".png")
+        plt.show()
