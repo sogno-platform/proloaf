2154316329a6b1377cbb336866a7d95810634e64
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2021-11-11 10:25:54 +0100

    fix pathes in shell scripts
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- shell/lint_script.sh
+++ shell/lint_script.sh
@@ -20,5 +20,5 @@
 # ==============================================================================
 pip install flake8
 pip install flake8-junit-report
-flake8 --output-file ${1}_lint.txt source/${1}.py
+flake8 --output-file ${1}_lint.txt src/${1}.py
 flake8_junit ${1}_lint.txt ${1}_lint.xml
