ee8438bf2582fa105d7e3d53467374642c05efe1
Harmanvir Singh <harmanvir.singh@rwth-aachen.de>
2022-01-05 21:56:12 +0100

    add device attribute in Transformer Class
    Signed-off-by: Harmanvir Singh <harmanvir.singh@rwth-aachen.de>

--- src/proloaf/models.py
+++ src/proloaf/models.py
@@ -314,10 +314,12 @@ class Transformer(nn.Module):
                  dropout: float=0.0,
                  forecast_horizon: float=168,
                  n_heads: int=6,
+                 device: str=None,
                  ):
         super(Transformer, self).__init__()
 
         self.forecast_horizon = forecast_horizon
+        self.device = device
 
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=n_heads, dropout=dropout,
                                                         batch_first=True)
@@ -335,12 +337,12 @@ class Transformer(nn.Module):
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
 
-    def forward(self, inputs_encoder, inputs_decoder, device='cuda'):
+    def forward(self, inputs_encoder, inputs_decoder):
         inputs_decoder = torch.cat(
-            (torch.zeros(inputs_decoder.shape[0], self.forecast_horizon, 1).to(device), inputs_decoder), 2).to(device)
-        inputs = torch.cat((inputs_encoder, inputs_decoder), 1).to(device)
+            (torch.zeros(inputs_decoder.shape[0], self.forecast_horizon, 1).to(self.device), inputs_decoder), 2).to(self.device)
+        inputs = torch.cat((inputs_encoder, inputs_decoder), 1).to(self.device)
 
-        mask = self._generate_square_subsequent_mask(inputs.shape[1]).to(device)
+        mask = self._generate_square_subsequent_mask(inputs.shape[1]).to(self.device)
 
         attention = self.transformer_encoder(inputs, mask)
         output = self.decoder(attention)
