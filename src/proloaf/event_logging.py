6a57ab7e94226b9a9b21d5ab2833cc5b0b9bff54
Gonca Gürses-Tran <gguerses@eonerc.rwth-aachen.de>
2022-03-24 15:59:51 +0100

    Finalize example tutorial - Updated, Closes #100
    Signed-off-by: Gonca Gürses-Tran <gguerses@eonerc.rwth-aachen.de>

--- src/proloaf/event_logging.py
+++ src/proloaf/event_logging.py
@@ -3,7 +3,7 @@ import logging
 import logging.config
 from proloaf.confighandler import read_config
 
-MAIN_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+MAIN_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def create_event_logger(
         name: str,
